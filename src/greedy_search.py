45a60c29934b38efb0748177335fc1cc58bcde4c
francis_85a <franperezcid85@gmail.com>
2025-11-26 22:01:35 +0100

    refactor(busqueda_greedy): :recycle: Cambiar nombre de variable

--- src/greedy_search.py
+++ src/greedy_search.py
@@ -32,25 +32,25 @@ def buscar_mejor_estacion(estaciones, estados_cubiertos):
             mejor_cobertura = len(nuevos_estados)
     return mejor_estacion, mejor_cobertura
 
-def busqueda_greedy (estaciones, estaciones_necesitadas):
+def busqueda_greedy (estaciones, estados_necesitados):
     
     estaciones_copia = estaciones.copy()
     estados_cubiertos = set()
-    estaciones_necesitadas = []
+    estados_necesitados = []
     ganancias = []
     numero_estados_cubiertos = []
 
-    while estados_cubiertos < estaciones_necesitadas:
+    while estados_cubiertos < estados_necesitados:
         mejor_cobertura, mejor_estacion = buscar_mejor_estacion(
             estaciones_copia, estados_cubiertos
         )
 
         if mejor_estacion:
             estados_cubiertos |= estaciones_copia[mejor_estacion]
-            estaciones_necesitadas.append(mejor_estacion)
+            estados_necesitados.append(mejor_estacion)
             numero_estados_cubiertos.append(len(estados_cubiertos))
             ganancias.append(mejor_cobertura)
             del estaciones_copia[mejor_estacion]
 
-    return (estaciones_necesitadas, numero_estados_cubiertos, ganancias, estados_cubiertos)
+    return (estados_necesitados, numero_estados_cubiertos, ganancias, estados_cubiertos)
 
