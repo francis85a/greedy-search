bc65272ed84ab99d62fa7b3c157d269f20691de1
francis_85a <franperezcid85@gmail.com>
2025-11-26 21:42:44 +0100

    test(test): :white_check_mark: Añadir casos test
    añadimos casos test para la busqueda greedy y otro mas para buscar la mejor estacion

--- test/test_greedy_search.py
+++ test/test_greedy_search.py
@@ -1,6 +1,8 @@
 import pytest
-from src.greedy_search import *
+from src.greedy_search import buscar_mejor_estacion
+from src.greedy_search import busqueda_greedy
 
+@pytest.mark.buscar_mejor_estacion
 def test_buscar_mejor_estacion():
     estados_cubiertos = set (["wa", "id"])
 
@@ -13,3 +15,41 @@ def test_buscar_mejor_estacion():
     mejor_estacion, mejor_cobertura = buscar_mejor_estacion(estaciones, estados_cubiertos)
     assert mejor_estacion == "ktwo"
     assert mejor_cobertura == 3
+
+@pytest.mark.buscar_mejor_estacion
+def test_buscar_mejor_estacion():
+    estados_cubiertos = set()
+    estaciones = {
+        "kone": set(["wa", "id", "mt"]),
+        "ktwo": set(["or", "nv", "ca"]),
+        "kthree": set(["nv", "ut"]),
+    }
+
+    mejor_estacion, mejor cobertura = buscar_mejor_estacion(estaciones, estados_cubiertos)
+
+    assert mejor_estacion in ["kone", "ktwo"]
+    assert mejor_cobertura == 3
+
+@pytest.mark.busqueda_greedy
+def test_busqueda_greedy():
+    
+    estados_necesitados = set(["id","nv","ut","mt","wa","or","ca","az"])
+    
+    estaciones = {
+        "kone": set(["id", "nv", "ut"]),
+        "ktwo": set(["wa", "id", "mt"]),
+        "kthree": set(["or", "nv", "ca"]),
+        "kfour": set(["nv", "ut"]),
+        "kfive": set(["ca", "az"]),
+    }
+
+    estaciones_necesitadas, numero_estados_cubiertos, ganancias, estados_cubiertos = (
+        busqueda_greedy(estaciones, estados_necesitados)
+    )
+
+    assert estados_cubiertos == estados_necesitados
+    assert set(estaciones_necesitadas) == set(["kone", "ktwo", "kthree", "kfive"])
+    assert numero_estados_cubiertos[-1] == len(estados_necesitados)
+    assert all(ganancia > 0 for ganancia in ganancias)
+    assert ganancias == sorted(ganancias, reverse=True)
+    
